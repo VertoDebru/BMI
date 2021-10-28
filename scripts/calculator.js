383c7382191caf4cb923e43e6cf2c0b40f8b976d
Tony Vervoot <t.vervoot@gmail.com>
2021-10-28 21:36:56 +0200

    Change layout style.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,9 +1,15 @@
-
-
 function calculate() {
     var weight = document.getElementById("weight").value;
     var height = document.getElementById("height").value;
-    const BMI = weight * height ** 2 /100000;
+    var cHeight = parseFloat(height)/100;
+    const BMI = weight * height ** 2 /100000;   // With weight in KG and height in m.
 
+    console.log("Weight : "+weight);
+    console.log("Height : "+height);
+    console.log("Float Height : "+parseFloat(height));
+    console.log("Convert Height : "+parseFloat(height)/100);
+    console.log("BMI : "+ weight * cHeight ** 2);
+    console.log("BMI : "+ weight * cHeight ** 2 /10);
+    console.log("BMI : "+ weight * height ** 2);
     console.log(parseFloat(BMI));
 }
